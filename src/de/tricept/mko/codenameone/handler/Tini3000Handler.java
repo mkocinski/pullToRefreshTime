374eb822d2480baba39597e0054132cbf06f418b
mkocinski <malte.kocinski@tricept.de>
2014-01-10 10:56:10 +0100

    added Exceptions from server

--- src/de/tricept/mko/codenameone/handler/Tini3000Handler.java
+++ src/de/tricept/mko/codenameone/handler/Tini3000Handler.java
@@ -7,7 +7,9 @@ import com.codename1.ui.Dialog;
 import de.tricept.mko.codenameone.NetManager;
 import de.tricept.mko.codenameone.requests.SpecificConnectionRequest;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by MKocinski on 08.01.14.
@@ -17,10 +19,16 @@ public class Tini3000Handler {
     String user;
     String pw;
 
-    static final String _nelsonURL = "http://nelson:9083/TINI_GATEWAY_HTTPRouter/ProxyServlet?REQUEST_NAME=";
-    static final String _reqOrigin = "REQUEST_ORIGIN=Client";
+    static final String NELSON_URL = "http://nelson:9083/TINI_GATEWAY_HTTPRouter/ProxyServlet?REQUEST_NAME=";
+    static final String REQ_ORIGIN= "REQUEST_ORIGIN=Client";
 
-    static final String _prefixServerError = "Error:";
+    static final String PREFIX_SERVER_ERROR = "Error:";
+
+    //Exceptions
+    static final String SERVER_EXCEPTION_FRONTEND = "FRONTEND_EXCEPTIONS";
+    static final String SERVER_EXCEPTION_BUSINESS_FUNCTION = "BUSINESS_FUNCTION_EXCEPTIONS";
+    static final String SERVER_EXCEPTION_BUSINESS_LAYER = "BUSINESS_LAYER_EXCEPTIONS";
+    static final String SERVER_EXCEPTION_SERVER = "SERVER_EXCEPTIONS";
 
     //parameters
     int year; //jahr
@@ -56,9 +64,9 @@ public class Tini3000Handler {
         nManager.manage();
         SpecificConnectionRequest testReq;
         if(testConnection)
-          testReq = new SpecificConnectionRequest(_nelsonURL+"processProjekteFuerPersonErmittelnREST&REQUEST_USER=_USER_&REQUEST_PASSWORT=_PWD_&"+_reqOrigin,false);
+          testReq = new SpecificConnectionRequest(NELSON_URL+"processProjekteFuerPersonErmittelnREST&REQUEST_USER=_USER_&REQUEST_PASSWORT=_PWD_&"+REQ_ORIGIN,false);
         else
-          testReq = new SpecificConnectionRequest(_nelsonURL+"processProjekteFuerPersonErmittelnREST&REQUEST_USER=" + user + "&REQUEST_PASSWORT=" + pw + "&"+_reqOrigin,false);
+          testReq = new SpecificConnectionRequest(NELSON_URL+"processProjekteFuerPersonErmittelnREST&REQUEST_USER=" + user + "&REQUEST_PASSWORT=" + pw + "&"+REQ_ORIGIN,false);
 
         testReq.setIsTestConnection(true);
         InfiniteProgress infinity = new InfiniteProgress();
@@ -79,15 +87,23 @@ public class Tini3000Handler {
         if(null == testResult || testResult.length() <= 0)
             return false;
 
-        if(testResult.startsWith(_prefixServerError))
+        if(testResult.startsWith(PREFIX_SERVER_ERROR))
         {
             Dialog.show("ServerError",testResult,"ok",null);
             return false;
         }
 
+
         try {
+            //SERVER_EXCEPTION_SERVER,SERVER_EXCEPTION_BUSINESS_FUNCTION,SERVER_EXCEPTION_BUSINESS_LAYER,SERVER_EXCEPTION_FRONTEND
             Result res = Result.fromContent(testResult, Result.JSON);
-            if(res.get("BUSINESS_FUNCTION_EXCEPTIONS") != null)
+            if(res.get("SERVER_EXCEPTION_SERVER") != null)
+                return false;
+            if(res.get("SERVER_EXCEPTION_BUSINESS_FUNCTION") != null)
+                return false;
+            if(res.get("SERVER_EXCEPTION_BUSINESS_LAYER") != null)
+                return false;
+            if(res.get("SERVER_EXCEPTION_FRONTEND") != null)
                 return false;
         }   catch (IllegalArgumentException illegalArgEx)
         {
